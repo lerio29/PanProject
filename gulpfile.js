65f5e88c9c6e90f0868ae8ea2cdbb842e26d7361
lerio29 <chicha29@gmail.com>
2015-07-24 16:35:04 +0200

    modif gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 'use strict';
 
-var gulp = require('gulp')
-var gutil = require('gulp-util')
-var plumber = require('gulp-plumber')
-var sass = require('gulp-sass')
-var browserify = require('gulp-browatchify')
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var browserify = require('gulp-browatchify');
 var source = require('vinyl-source-stream');
-var git = require('gulp-git')
+var git = require('gulp-git');
+var livereload = require('gulp-livereload');
 
-
-
-gulp.task('styles', function() {
-  return gulp.src('./public/stylesheets/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./public/stylesheets/'))
+//tache de sass
+gulp.task('styles', function () {
+    return gulp.src('./public/stylesheets/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./public/stylesheets/'));
 });
 
 
@@ -25,33 +25,38 @@ gulp.task('styles', function() {
 //      .pipe(gulp.dest('./public/stylesheets/'))
 //})
 
-gulp.task('watch', ['styles', 'browserify'], function() {
-  gulp.watch('./public/stylesheets/*.scss', ['styles'])
-  gulp.watch('./index.js', ['browserify'])
+gulp.task('watch', ['styles', 'browserify'], function () {
+    var server = livereload();
+  
+    gulp.watch('./public/stylesheets/*.scss', ['styles']);
+    gulp.watch('./index.js', ['browserify']);
+    gulp.watch(['*.js']).on('change', function (event) {
+		server.changed(event.path);
+    });
 });
 
 
-gulp.task('git',function(){
-   gulp.src('./*')
-   .pipe(git.add());
+/*gulp.task('git',function () {
+    gulp.src('./*')
+    .pipe(git.add());
 
-  gulp.src('./*')
-  .pipe(git.commit('commit auto par gulp'));
+    gulp.src('./*')
+    .pipe(git.commit('commit auto par gulp'));
 
-  return git.push('origin', 'master');
+    return git.push('origin', 'master');
 
-});
+});*/
 
-gulp.task('browserify', function() {
+gulp.task('browserify', function () {
 
-  return gulp.src('./index.js')
+    return gulp.src('./index.js')
     .pipe(browserify())
     .pipe(source('bundleBrowserified.js'))
-    .pipe(gulp.dest('./public'))
-
+    .pipe(gulp.dest('./public'));
 });
 
 
-gulp.task('default', ['styles', 'browserify', 'styles'], function() {
+
+gulp.task('default', ['browserify', 'styles'], function () {
   
 });
